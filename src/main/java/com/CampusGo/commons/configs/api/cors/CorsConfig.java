9ba3d66b4e08a7b1ebcde62dd1aae7aafe15e3e6
rafapsjs <rafaelcorredorgambin1@gmail.com>
2025-04-07 08:02:40 -0500

    changes cors

--- src/main/java/com/CampusGo/commons/configs/api/cors/CorsConfig.java
+++ src/main/java/com/CampusGo/commons/configs/api/cors/CorsConfig.java
@@ -6,19 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+public class CorsConfig  implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplicar CORS para todas las rutas
+                .allowedOrigins("http://localhost:8080") // Permitir solicitudes desde este origen (ajusta según tu entorno)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir estos métodos HTTP
+                .allowedHeaders("*") // Permitir cualquier cabecera
+                .allowCredentials(true); // Permitir enviar credenciales (cookies, cabeceras de autorización, etc.)
     }
 }
